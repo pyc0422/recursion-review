7a8d12bf9f75ea67ab6197d09c8ff2a3471f1cfe
bbissing <bbissing101@gmail.com>
2022-06-04 13:37:00 -0500

    Complete stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,38 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = '';
+
+  if (obj === null) {
+    return '' + obj;
+  }
+  if (typeof obj === 'number') {
+    return '' + obj;
+  }
+  if (typeof obj === 'boolean') {
+    return '' + obj;
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+    _.each(obj, function(elem) {
+      if (obj.indexOf(elem) === obj.length - 1) {
+        result += stringifyJSON(elem);
+      } else {
+        result += stringifyJSON(elem) + ',';
+      }
+    });
+    return '[' + result + ']';
+  } else if (typeof obj === 'object') {
+    _.each(obj, function(value, key) {
+      if (key !== 'functions' && key !== 'undefined') {
+        result += stringifyJSON(key) + ':' + stringifyJSON(value) + ',';
+      }
+    });
+    // delete the last  ',' in result
+    return '{' + result.slice(0, -1) + '}';
+  }
+  console.log(result);
+  return result;
 };
